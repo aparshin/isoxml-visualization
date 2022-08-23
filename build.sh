74907ba84c89b3bb789bdf91af1f99e23848c244
Frank Wiebeler <frank.wiebeler@dev4Agriculture.de>
2022-08-23 13:25:03 +0200

    Adjust Build Script

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
+docker rmi isoxml-visualization --force
 DOCKER_BUILDKIT=1 docker build --no-cache --ssh github=/home/frank/.ssh/rsa_id_github -t isoxml-visualization .
 docker run -p 3000:8000 --name isoxml-visualization --network net -e VIRTUAL_HOST="isoxml-viewer.dev4ag.com" -e LETSENCRYPT_HOST="isoxml-viewer.dev4ag.com" -d isoxml-visualization &
